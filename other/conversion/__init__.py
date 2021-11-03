cee7bc562e743a44e31ad7f208c7238470e8b009
Martijn Stegeman <martijn@stgm.nl>
2021-11-03 18:04:42 +0100

    testtest

--- other/conversion/__init__.py
+++ other/conversion/__init__.py
@@ -22,32 +22,41 @@ def testC0205():
     check50.run("./conversion").stdin("C").stdin("0").stdin("20").stdin("5").stdout(correct, correct, False).exit(0)
 
 
+@check50.check(compiles)
+def testF0102():
+    """input of F, 0, 10, 2 yields a well-formatted table"""
+    correct = open("F0102.txt").read()
+    check50.run("./conversion").stdin("F").stdin("0").stdin("10").stdin("2").stdout(correct, correct, False).exit(0)
 
 
-# @check50.check(compiles)
-# def test_reject_negative():
-#     """rejects a negative input like -1"""
-#     check50.run("./conversion").stdin("-1").reject()
-#
-#
-# @check50.check(test_reject_negative)
-# def test_041_after_negative():
-#     """erroneous input of -1 and then input of 0.41 yields output of 4"""
-#     check50.run("./conversion").stdin("-1").stdin("0.41").stdout(coins(4), "4\n").exit(0)
-#
-#
-# @check50.check(compiles)
-# def test_reject_foo():
-#     """rejects a non-numeric input of "foo" """
-#     check50.run("./conversion").stdin("foo").reject()
-#
-#
-# @check50.check(compiles)
-# def test_reject_empty():
-#     """rejects a non-numeric input of "" """
-#     check50.run("./conversion").stdin("").reject()
-#
-#
-# def coins(num):
-#     # regex that matches `num` not surrounded by any other numbers (so coins(2) won't match e.g. 123)
-#     return fr"(?<!\d){num}(?!\d)"
+@check50.check(compiles)
+def testF0102():
+    """input of F, 100, 0, 3 yields a well-formatted table"""
+    correct = open("F10003.txt").read()
+    check50.run("./conversion").stdin("F").stdin("100").stdin("0").stdin("3").stdout(correct, correct, False).exit(0)
+
+
+@check50.check(compiles)
+def testF193():
+    """input of F, 1, 9, 3 yields a well-formatted table"""
+    correct = open("F193.txt").read()
+    check50.run("./conversion").stdin("F").stdin("1").stdin("9").stdin("3").stdout(correct, correct, False).exit(0)
+
+
+@check50.check(compiles)
+def test_reject_incorrect_unit():
+    """rejects incorrect unit"""
+    check50.run("./conversion").stdin("v").reject()
+
+
+@check50.check(compiles)
+def test_reject_negative():
+    """rejects a negative input like -1 for starting temperature"""
+    check50.run("./conversion").stdin("F").stdin("-1").reject()
+
+
+@check50.check(compiles)
+def testF093_after_negative():
+    """input of v, F, 0, 9, -3, 0, 3 yields a well-formatted table"""
+    correct = open("F093.txt").read()
+    check50.run("./conversion").stdin("v").stdin("F").stdin("0").stdin("9").stdin("-3").stdin("0").stdin("3").stdout(correct, correct, False).exit(0)
